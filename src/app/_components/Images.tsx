0e83ffc50750e74f2f48a08731541f1333a299a5
David <davidbotos60@yahoo.ro>
2025-03-02 16:23:09 +0200

    improve images

--- src/app/_components/Images.tsx
+++ src/app/_components/Images.tsx
@@ -1,25 +1,32 @@
 "use client";
 
-import { Image } from "@prisma/client";
+import { Image as TImage } from "@prisma/client";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import Image from "next/image";
 
 type CarouselProps = {
-  images: Image[];
+  images: TImage[];
 };
 
 export const Images = ({ images }: CarouselProps) => {
   if (!images.length) return <div className="skeleton h-60 w-full"></div>;
   return (
     <Carousel
-      className="overflow-hidden rounded-lg lg:max-w-sm"
+      className="w-full overflow-hidden rounded-lg lg:max-w-sm"
       showThumbs={false}
       showStatus={false}
       dynamicHeight={true}
     >
       {images.map(({ url }, index) => (
         <div key={index} className="relative w-full">
-          <img src={url} className="w-full" />
+          <Image
+            width="100"
+            height="100"
+            src={url}
+            className="w-full"
+            alt={url}
+          />
         </div>
       ))}
     </Carousel>
