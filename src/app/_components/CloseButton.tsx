83023697198d0b4b63a7c61d7375cbee3ac06d50
David <davidbotos60@yahoo.ro>
2025-07-16 23:31:04 +0300

    change button to icon

--- src/app/_components/CloseButton.tsx
+++ src/app/_components/CloseButton.tsx
@@ -1,7 +1,9 @@
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
 export const XButton = () => (
   <form method="dialog">
-    <button className="btn btn-circle btn-ghost btn-lg absolute top-2 right-2">
-      ✕
+    <button className="btn btn-circle btn-ghost btn-sm absolute top-2 right-2">
+      <XMarkIcon />
     </button>
   </form>
 );
