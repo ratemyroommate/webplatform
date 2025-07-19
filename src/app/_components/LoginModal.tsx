7cc95e6cdf15ae0a1b5dea03f525e7be26a2f152
Zolcsák Gergő <zolcsakgergo2001@gmail.com>
2025-07-19 15:06:30 +0200

    add CTA to login modal

--- src/app/_components/LoginModal.tsx
+++ src/app/_components/LoginModal.tsx
@@ -11,6 +11,9 @@ export const LoginModal = () => {
     <dialog id="login-modal" className="modal">
       <div className="modal-box py-16">
         <XButton />
+        <p className="mb-4 text-center text-lg">
+          Just a quick login before we continue.
+        </p>
         <Link href="/api/auth/signin" className="btn btn-primary w-full">
           Login
         </Link>
