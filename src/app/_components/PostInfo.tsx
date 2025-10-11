6b0e6e0e4cef9fc8051185b27acab008131bd918
David <davidbotos60@yahoo.ro>
2025-10-11 22:43:34 +0300

    add tooltips for post info

--- src/app/_components/PostInfo.tsx
+++ src/app/_components/PostInfo.tsx
@@ -12,19 +12,19 @@ export const PostInfo = ({ post }: PostInfoProps) => {
     <div className="grid grid-cols-2 grid-rows-2 gap-2">
       <PostPrice price={post.price} />
       <div className="flex items-center">
-        <div className="flex items-center gap-2">
+        <div className="tooltip flex items-center gap-2" data-tip="Város">
           <MapPinIcon width={24} color="brown" />
           <b>{getLocationLabel(post.location)}</b>
         </div>
       </div>
       <div className="flex">
-        <div className="flex items-center gap-2">
+        <div className="tooltip flex items-center gap-2" data-tip="Kor">
           <UserIcon width={24} color="red" />
           <b>{getAgeLabel(post.age)}</b>
         </div>
       </div>
       <div className="flex">
-        <div className="flex items-center gap-2">
+        <div className="tooltip flex items-center gap-2" data-tip="Nem">
           <img src="/gender-fluid.png" width={25} />
           <b>{getGenderLabel(post.gender)}</b>
         </div>
--- src/app/_components/PostPrice.tsx
+++ src/app/_components/PostPrice.tsx
@@ -5,7 +5,10 @@ type PostPriceProps = {
 };
 
 export const PostPrice = ({ price }: PostPriceProps) => (
-  <div className="flex flex-1 items-center gap-2">
+  <div
+    className="tooltip flex flex-1 items-center gap-2"
+    data-tip="Bérleti díj"
+  >
     <BanknotesIcon width={24} color="green" />
     <b>{price}k</b>
     <span className="text-sm text-gray-500">ft/hónap</span>
